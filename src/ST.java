8e4b04ec897bf76230cb6b6ee4c745d0aa2deb26
JeffYnag <j910jeff@gmail.com>
2017-12-09 11:38:34 +0800

    3

--- src/ST.java
+++ src/ST.java
@@ -4,6 +4,8 @@ public class ST {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         *//abcdeffghijklmnopqrstuvwxyz
+        
+        1321331316465416541
 	}
 
 }
